e31ecf5e318dbceaeab4b84bf499698b5a873752
Josephine Chan <jcf1996chan@gmail.com>
2023-12-15 23:52:24 -0500

    Retrieves existing rooms from api

--- src/frontend/src/Rooms.tsx
+++ src/frontend/src/Rooms.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
+interface KTVroom {
+  name: string;
+  online: number;
+}
+
 function Rooms() {
   const navigate = useNavigate();
+  const [ktvrooms, setKTVrooms] = useState<KTVroom[]>([]);
 
-  const handleNewChatroomClick = () => {
-    const newChatroomId = uuidv4();
-    navigate(`/${newChatroomId}`);
+  const fetchKTVrooms = async () => {
+    try {
+      const response = await fetch('https://sinsphere-api.nahemah.com/api/stats');
+      const data: { rooms: KTVroom[] } = await response.json();
+
+      if (data.rooms && Array.isArray(data.rooms)) {
+        setKTVrooms(data.rooms);
+      }
+    } catch (error) {
+      console.error('Error fetching ktvrooms:', error);
+    }
   };
+
+  const handleNewKTVroomClick = async () => {
+    const newKTVroomId = uuidv4();
+    navigate(`/${newKTVroomId}`);
+
+    // Fetch ktvrooms after creating a new room
+    await fetchKTVrooms();
+  };
+
+  // Fetch ktvrooms when mounting
+  useEffect(() => {
+    fetchKTVrooms();
+  }, []);
 
   return (
     <div>
-      <h1>Chatrooms</h1>
-      <button onClick={handleNewChatroomClick}>New Chatroom</button>
+      <h1>KTV Rooms</h1>
+      <button onClick={handleNewKTVroomClick}>New KTVroom</button>
       <ul>
-        <li>
-          <Link to="/123">Chatroom 123</Link>
-        </li>
-        <li>
-          <Link to="/456">Chatroom 456</Link>
-        </li>
+        {ktvrooms.map((room) => (
+          <li key={room.name}>
+            <Link to={`/${room.name}`}>{`KTVroom ${room.name} (${room.online} online)`}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
