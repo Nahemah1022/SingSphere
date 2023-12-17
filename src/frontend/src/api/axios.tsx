866518d0055070018a93283ab413d2281371a3df
Josephine Chan <jcf1996chan@gmail.com>
2023-12-17 10:48:02 -0500

    Enqueue still doesn't work, check if frontend works on AWS

--- src/frontend/src/api/axios.tsx
+++ src/frontend/src/api/axios.tsx
@@ -1,89 +1,101 @@
 import React, { useState } from 'react';
-import axios, {AxiosRequestConfig, AxiosResponse} from 'axios';
-import {sign} from 'aws4';
-//import { aws4Interceptor } from "aws4-axios";
+import axios, { AxiosRequestConfig, AxiosResponse, Method} from 'axios';
+//import { sign} from 'aws4';
+import {aws4Interceptor} from 'aws4-axios';
 import AWSCredentials from '../aws_creds_local';
 
 interface Song {
   url: string;
-  name: string;
+  search_term: string;
   labels: string[];
 }
 
-// Only for POST because GET doens't have CORS set
+//interface SignedRequest {
+//  method: Method;
+//  //service: string;
+//  region: string;
+//  host: string;
+//  headers: Record<string, string>;
+//  body: string;
+//}
+
 const client = axios.create();
 
-//const interceptor = aws4Interceptor({
-//  options: {
-//    region: "us-east-1",
-//	service: "execute-api",
+const interceptor = aws4Interceptor({
+  options: {
+    region: "us-east-1",
+	service: "execute-api",
 //	assumeRoleArn: "arn:aws:iam::601912694676:user/Josephine"
-//  },
-//  credentials: AWSCredentials
-//});
+  },
+//  credentials: AWSCredentials,
+});
 
-//client.interceptors.request.use(interceptor);
+client.interceptors.request.use(interceptor);
 
 const SearchPage = () => {
   const [songName, setSongName] = useState('');
-  const [searchResults, setSearchResults] = useState<Song[]>([]);
+  const [songs, setSongs] = useState<Song[]>([]);
 
   const handleSearch = async () => {
     try {
       const response = await axios.get(
         `https://zuooeb1uui.execute-api.us-east-1.amazonaws.com/Dev/GET/?song=${songName}`
       );
-	  console.log(response);
+      console.log(response);
 
-      setSearchResults(response.data.results);
+      setSongs(response.data.results);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
-  // Explicitly define the song parameter type
-  const queueSong = async (song: Song) => {
+  const queueSong = async (result: Song) => {
     try {
-		const path = `/Dev/POST/final-music`;
-		const url = `https://zuooeb1uui.execute-api.us-east-1.amazonaws.com/Dev/POST/final-music`;
+      const path = '/Dev/POST/final-music';
+      const url = 'https://zuooeb1uui.execute-api.us-east-1.amazonaws.com/Dev/POST/final-music';
+	  //const url = 'https://zuooeb1uui.execute-api.us-east-1.amazonaws.com/';
 
-		const requestBody = {
-			song: song.name,
-			room: '123',
-		};
+      const requestBody = {
+        song: result.search_term,
+        room: '123',
+      };
 
-		// Convert requestBody to Base64
-		const base64RequestBody = Buffer.from(JSON.stringify(requestBody)).toString('base64');
+      const base64RequestBody = Buffer.from(JSON.stringify(requestBody)).toString('base64');
 
-		// Create a request object
-		const request = {
-			host: 'zuooeb1uui.execute-api.us-east-1.amazonaws.com',
-			method: 'POST',
-			url: url,
-			data: base64RequestBody,
-			headers: {
-				'content-type': 'application/json'
-			}
-		};
-
-		// Sign the request
-		const signedRequest = sign(
-			request,
-			AWSCredentials
-		);
-
-		if (signedRequest.headers) {
-			delete signedRequest.headers['Host'];
-			delete signedRequest.headers['Content-Length'];
+	  const response = await axios({
+		method: 'POST',
+		url: url,
+		data: base64RequestBody,
+		headers: {
+			'Content-Type': 'text/plain'
 		}
+	  })
 
-		// Use the signed request for the API call
-		const response: AxiosResponse = await client(request);
+    //  const request = {
+    //    //host: 'zuooeb1uui.execute-api.us-east-1.amazonaws.com',
+	//	host,
+    //    method: 'POST',
+	//	region: 'us-east-1',
+    //    body: base64RequestBody,
+    //    headers: {
+    //      'content-type': 'application/json',
+    //    },
+    //  };
 
-		console.log('Song queued successfully:', response.data);
-	} catch (error) {
-		console.error('Error queuing song:', error);
-	}
+	//  let signedRequest: AxiosRequestConfig = {}
+    //  signedRequest.data = sign(request, AWSCredentials);
+
+	  //  if (signedRequest.headers) {
+		//	delete signedRequest.headers['Host'];
+		//    delete signedRequest.headers['Content-Length'];  }
+
+    //  // Use the axios request for the API call
+    //  let response: AxiosResponse = await axios(signedRequest);
+
+      console.log('Song queued successfully:', response.data);
+    } catch (error) {
+      console.error('Error queuing song:', error);
+    }
   };
 
   return (
@@ -102,9 +114,9 @@ const SearchPage = () => {
       <div>
         <h2>Search Results:</h2>
         <ul>
-          {searchResults.map((result, index) => (
+          {songs.map((result, index) => (
             <li key={index}>
-              {result.name} - {result.labels.join(', ')}
+              {result.search_term} - {result.labels.join(', ')}
               <button onClick={() => queueSong(result)}>Queue Song</button>
             </li>
           ))}
